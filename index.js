02a982ac79be2314532d4bb76f1499003c72cb87
Luis Anibal Fabio <luis.fabio@mercadolibre.com>
2019-05-19 21:15:45 -0300

    enable cors

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const Schedule = require('./schedule');
 
 const server = restify.createServer();
 
+server.use(restify.CORS());
+
+server.opts(/.*/, function (req,res,next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", req.header("Access-Control-Request-Method"));
+    res.header("Access-Control-Allow-Headers", req.header("Access-Control-Request-Headers"));
+    res.send(200);
+    return next();
+});
 
 //Middleware
 server.use(restify.plugins.bodyParser());
