f79618c91e06a4d702a7fd035610063558a7afdf
Luis Anibal Fabio <luis.fabio@mercadolibre.com>
2019-05-20 21:10:19 -0300

    fix

--- routes/bings.js
+++ routes/bings.js
@@ -42,33 +42,33 @@ module.exports = server => {
             return next(new errors.InvalidContentError("Expects 'application/json'"));
         }
 
-        let buses = await Bus.find({},
-            ['imei', 'next_stop', "eta_next_stop", "status", "direction"],
-            {
-                sort: {
-                    next_stop: 1
-                }
-            }
-        );
-
-        let stops = await Stop.find({},
-            ['num_stop', 'name', "eta_stop", "status"],
-            {
-                sort: {
-                    num_stop: 1
-                }
-            }
-        );
-        for (let i = bing.id_stop; i !== 0; i--) { //TODO: cambiar esto teniendo en cuenta el retorno
-            let bus = await Utils.findObjectByKey(buses, "next_stop", i); //TODO: hacer funcion dedicada
-            let stop = await Utils.findObjectByKey(stops, "num_stop", i); //TODO: hacer funcion dedicada
-            stops_sum += stop.eta_stop;
-
-            if (bus !== null && bus.eta_next_stop + stops_sum > bing.time) {
-                bing.bus_assign = bus.imei;
-                break;
-            }
-        }
+        // let buses = await Bus.find({},
+        //     ['imei', 'next_stop', "eta_next_stop", "status", "direction"],
+        //     {
+        //         sort: {
+        //             next_stop: 1
+        //         }
+        //     }
+        // );
+        //
+        // let stops = await Stop.find({},
+        //     ['num_stop', 'name', "eta_stop", "status"],
+        //     {
+        //         sort: {
+        //             num_stop: 1
+        //         }
+        //     }
+        // );
+        // for (let i = bing.id_stop; i !== 0; i--) { //TODO: cambiar esto teniendo en cuenta el retorno
+        //     let bus = await Utils.findObjectByKey(buses, "next_stop", i); //TODO: hacer funcion dedicada
+        //     let stop = await Utils.findObjectByKey(stops, "num_stop", i); //TODO: hacer funcion dedicada
+        //     stops_sum += stop.eta_stop;
+        //
+        //     if (bus !== null && bus.eta_next_stop + stops_sum > bing.time) {
+        //         bing.bus_assign = bus.imei;
+        //         break;
+        //     }
+        // }
 
 
         try {
