556957d5817789f520801f517ed9925f54df902f
Luis Anibal Fabio <luis.fabio@mercadolibre.com>
2019-06-14 19:08:59 -0300

    recorrido

--- models/Bus.js
+++ models/Bus.js
@@ -14,26 +14,26 @@ const BusSchema = new mongoose.Schema({
     },
     long: {
         type: Number,
-        required: true,
+        required: false,
         trim: true
     }, 
     next_stop: {
         type: Number,
         default: 1,
-        required: true,
+        required: false,
     },
     eta_next_stop: {
         type: Number,
-        required: true,
+        required: false,
     },
     status: {
         type: String,
-        required: true,
+        required: false,
     },
     attempts: {
         type: Number,
         default: 0,
-        required: true,
+        required: false,
     },
 
 });
--- routes/buses.js
+++ routes/buses.js
@@ -10,76 +10,88 @@ module.exports = server => {
     const STATUS_ON_CHANGE = 'on_change';
     const MAX_ATTEMPS = '60';
 
+    // server.get('/buses', async (req, res, next) => {
+    //     const {imei, lat, long} = req.query;
+    //     let bus;
+    //     let nextStop;
+    //     let distanceBusToStop = 0.0;
+    //     let here;
+    //
+    //     bus = await Bus.findOne({imei: req.query.imei});
+    //
+    //     if (bus) {
+    //         bus.lat = req.query.lat;
+    //         bus.long = req.query.long;
+    //         bus.status = STATUS_ON;
+    //
+    //         nextStop = await Stop.findOne({num_stop: bus.next_stop});
+    //
+    //
+    //         if (bus.attempts === 0) {
+    //             try {
+    //                 here = await Utils.rget(stop, bus);
+    //                 bus.eta_next_stop = here.travelTime;
+    //             } catch (ignored) {}
+    //             bus.attempts = MAX_ATTEMPS;
+    //         } else {
+    //             bus.attempts --;
+    //         }
+    //
+    //         if (bus.eta_next_stop === undefined) {
+    //             bus.eta_next_stop = 100;
+    //         }
+    //         console.log(bus.lat, bus.long, nextStop.lat, nextStop.long)
+    //         distanceBusToStop = await Utils.distance(bus, nextStop);
+    //         console.log(distanceBusToStop, nextStop.long_stop)
+    //
+    //         if (distanceBusToStop < nextStop.long_stop) {
+    //             bus.status = STATUS_ON_CHANGE;
+    //         }
+    //
+    //         console.log(bus.status && distanceBusToStop >= nextStop.long_stop)
+    //         if (bus.status === STATUS_ON_CHANGE && distanceBusToStop >= nextStop.long_stop) {
+    //             bus.next_stop++;
+    //             bus.status = STATUS_ON;
+    //         }
+    //
+    //         bus.save();
+    //         res.send(200);
+    //     } else {
+    //         const stop = await Stop.findOne({ num_stop: 0 });
+    //         bus = new Bus({imei, lat, long, status: STATUS_INITIAL, nextStop: 0});
+    //
+    //         try {
+    //             here = await Utils.rget(stop, bus); //TODO: suponer que se inicia del inicio (Parada inicial = 0)
+    //             bus.eta_next_stop = here.travelTime;
+    //         } catch (ignored) {
+    //
+    //         }
+    //
+    //         if (bus.eta_next_stop === undefined) {
+    //             bus.eta_next_stop = 100;
+    //         }
+    //
+    //         try {
+    //             await bus.save();
+    //             res.send(201);
+    //         } catch (err) {
+    //             return next(new errors.InternalError(err.message));
+    //         }
+    //     }
+    //
+    //     next();
+    // });
+
     server.get('/buses', async (req, res, next) => {
-        const {imei, lat, long} = req.query;
-        let bus;
-        let nextStop;
-        let distanceBusToStop = 0.0;
-        let here;
-
-        bus = await Bus.findOne({imei: req.query.imei});
-
-        if (bus) {
-            bus.lat = req.query.lat;
-            bus.long = req.query.long;
-            bus.status = STATUS_ON;
-
-            nextStop = await Stop.findOne({num_stop: bus.next_stop});
-
-
-            if (bus.attempts === 0) {
-                try {
-                    here = await Utils.rget(stop, bus);
-                    bus.eta_next_stop = here.travelTime;
-                } catch (ignored) {}
-                bus.attempts = MAX_ATTEMPS;
-            } else {
-                bus.attempts --;
-            }
-
-            if (bus.eta_next_stop === undefined) {
-                bus.eta_next_stop = 100;
-            }
-            console.log(bus.lat, bus.long, nextStop.lat, nextStop.long)
-            distanceBusToStop = await Utils.distance(bus, nextStop);
-            console.log(distanceBusToStop, nextStop.long_stop)
-
-            if (distanceBusToStop < nextStop.long_stop) {
-                bus.status = STATUS_ON_CHANGE;
-            }
-
-            console.log(bus.status && distanceBusToStop >= nextStop.long_stop)
-            if (bus.status === STATUS_ON_CHANGE && distanceBusToStop >= nextStop.long_stop) {
-                bus.next_stop++;
-                bus.status = STATUS_ON;
-            }
-
-            bus.save();
-            res.send(200);
-        } else {
-            const stop = await Stop.findOne({ num_stop: 0 });
-            bus = new Bus({imei, lat, long, status: STATUS_INITIAL, nextStop: 0});
-
-            try {
-                here = await Utils.rget(stop, bus); //TODO: suponer que se inicia del inicio (Parada inicial = 0)
-                bus.eta_next_stop = here.travelTime;
-            } catch (ignored) {
-
-            }
-
-            if (bus.eta_next_stop === undefined) {
-                bus.eta_next_stop = 100;
-            }
-
-            try {
-                await bus.save();
-                res.send(201);
-            } catch (err) {
-                return next(new errors.InternalError(err.message));
-            }
+        try {
+            const {imei, lat, long} = req.query;
+            bus = new Bus({imei, lat, long});
+            await bus.save();
+            res.send(201);
+            next();
+        } catch(err) {
+            return next(new errors.InvalidContentError(err));
         }
-
-        next();
     });
 
     server.get('/list-buses', async (req, res, next) => {
